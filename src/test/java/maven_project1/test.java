422776cc8549a7196ba3c337a01a45beb0cc5abe
aminous129 <118860624+aminous129@users.noreply.github.com>
2023-03-22 21:30:00 -0400

    Update test.java

--- src/test/java/maven_project1/test.java
+++ src/test/java/maven_project1/test.java
@@ -2,16 +2,22 @@ package maven_project1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test {
-
+	public static ChromeOptions options;
+	public static WebDriver driver;
+	public static WebDriverWait wait;
 	public static void main(String[] args) {
-		WebDriver driver= new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
+		options = new ChromeOptions();
+	      options.addArguments("--start-maximized");
+	      options.addArguments("--remote-allow-origins=*");
+	      driver = new ChromeDriver(options);
+	      driver.get("https://www.makemytrip.com/?ccde=us");
 
 		//Please press Ctrl+S save your program
-		driver.get("http://tutorialsninja.com/demo");
+	
 
 	}
 
